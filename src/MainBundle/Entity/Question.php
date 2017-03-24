11f272b7db62e5885813accaa45114fef16ed7a9
Peter <peterduits@live.nl>
2017-03-24 13:41:12 +0100

    Question Entity created

---
+++ src/MainBundle/Entity/Question.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace MainBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Question
+ *
+ * @ORM\Table(name="question")
+ * @ORM\Entity(repositoryClass="MainBundle\Repository\QuestionRepository")
+ */
+class Question
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="enqueteId", type="integer")
+     */
+    private $enqueteId;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Question
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set enqueteId
+     *
+     * @param integer $enqueteId
+     *
+     * @return Question
+     */
+    public function setEnqueteId($enqueteId)
+    {
+        $this->enqueteId = $enqueteId;
+    
+        return $this;
+    }
+
+    /**
+     * Get enqueteId
+     *
+     * @return integer
+     */
+    public function getEnqueteId()
+    {
+        return $this->enqueteId;
+    }
+}
+
---
+++ src/MainBundle/Form/QuestionType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace MainBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class QuestionType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('title')->add('enqueteId')        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'MainBundle\Entity\Question'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'mainbundle_question';
+    }
+
+
+}
