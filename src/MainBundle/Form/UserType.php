289504d6d120d170ac8d5c7b5f55193e4b1b68a3
M44RTW1NT1GK4R4KT3R5 <1020156@idcollege.nl>
2017-04-05 15:13:55 +0200

    Added Users crud

---
+++ src/MainBundle/Form/UserType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace MainBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('firstname')->add('lastname')->add('gender')->add('birthdate')->add('address')->add('postal')->add('city')->add('phone')->add('bankaccount')        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'MainBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'mainbundle_user';
+    }
+
+
+}
