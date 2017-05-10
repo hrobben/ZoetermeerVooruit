df6c82bede461b7714313ddd32156c0aab890452
M44RTW1NT1GK4R4KT3R5 <1020156@idcollege.nl>
2017-05-10 08:44:14 +0200

    opslaan & nieuw knop bij choice & ids vervangen voor titles bij questions

--- src/MainBundle/Form/ChoiceType.php
+++ src/MainBundle/Form/ChoiceType.php
@@ -3,6 +3,7 @@
 namespace MainBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -19,7 +20,13 @@ class ChoiceType extends AbstractType
                 ])
             ->add('choice', null, [
                 'label' => "Antwoord",
-                ]);
+                ])
+            ->add('save_new', SubmitType::class, array(
+                'validation_groups' => false,
+                'label'=>'Opslaan & nieuw'
+            ));
+        
+
     }
     
     /**
